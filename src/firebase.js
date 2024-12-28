546560cb2fd39ff21cbff46b7729432275d129e0
SanthoshJayavelu22 <santhoshrakey2@gmail.com>
2024-12-28 15:54:58 +0530

    updated 2

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 
 
@@ -15,5 +14,4 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
